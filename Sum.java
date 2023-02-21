5a7ed9b799c84134933046701ed233563fd54ba5
Vansham Rathore <vanshamrathore@gmail.com>
2023-02-21 14:09:53 +0530

    Aman.java

---
+++ Sum.java
@@ -0,0 +1,13 @@
+public class um {
+
+    public static void main(String[] args) {
+
+        int x = 89;
+
+        int y = 43;
+
+        int sum = x + y;
+
+        System.out.println("My Name is khana" + sum);
+    }
+}
