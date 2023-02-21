48454e42f8a7ad5f0a753b92c08d1aad8fe04448
Vansham Rathore <vanshamrathore@gmail.com>
2023-02-21 13:06:42 +0530

    added multiplay.java

---
+++ multiplay.java
@@ -0,0 +1,13 @@
+public class multiplay {
+
+    public static void main(String[] args) {
+
+        int x = 89;
+
+        int y = 43;
+
+        int mul = x * y;
+
+        System.out.println("My Name is khana" + mul);
+    }
+}
